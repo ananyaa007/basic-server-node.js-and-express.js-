2a79f13d9a9408dcf6f220666afcc51b26ae95a9
Ananya Awasthi <101282178+ananyaa007@users.noreply.github.com>
2024-12-08 13:57:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for the root URL that serves the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
